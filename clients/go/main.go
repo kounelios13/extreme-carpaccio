a901422274f8833010787b5be006f890410565d7
harry_k3 <harry_k3@hotmail.com>
2018-01-26 22:35:56 +0200

    thinkpad2 team in GO!

--- clients/go/main.go
+++ clients/go/main.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os"
 
+	"errors"
 	"fmt"
 )
 
@@ -60,9 +61,80 @@ func handler(rw http.ResponseWriter, req *http.Request) {
 	fmt.Printf("Got order: %#v\n", order)
 
 	rw.Header().Add("Content-Type", "application/json")
+	
+	encoder := json.NewEncoder(rw)
+	total, err := totalCost(order)
+	if err != nil {
+		rw.WriteHeader(400)
+		return
+	}
+
+	total, err = taxReduction(total, order.Country)
+	if err != nil {
+		rw.WriteHeader(400)
+		return
+	}
+
+	if order.Reduction == "STANDARD" {
+		total = reduction(total)
+	}
+
 	rw.WriteHeader(200)
-	//	encoder := json.NewEncoder(rw)
-	//	encoder.Encode(Reply{0})
+	encoder.Encode(Reply{float32(total)})
+}
+
+func totalCost(order Order) (float64, error) {
+	sum := 0.0
+	if len(order.Prices) == 0 || len(order.Prices) != len(order.Quantities) {
+		return 0, errors.New("invalid")
+	}
+	for i, _ := range order.Prices {
+		sum += float64(order.Prices[i] * float32(order.Quantities[i]))
+	}
+
+	return sum, nil
+}
+
+func taxReduction(total float64, country string) (float64, error) {
+	c := 0
+	c = countries()[country]
+	if len(country) == 0 || c == 0 {
+		return total, errors.New("invalid")
+	}
+	return total + total*(float64(countries()[country])/100), nil
+}
+
+func countries() map[string]int {
+	return map[string]int{
+		"DE": 20,
+		"UK": 21,
+		"FR": 20,
+		"IT": 25,
+		"ES": 19,
+		"PL": 21,
+		"RO": 20,
+		"NL": 20,
+		"BE": 24,
+		"EL": 20,
+		"CZ": 19,
+		"PT": 23,
+		"HU": 27,
+		"SE": 23,
+		"AT": 22,
+		"BJ": 23,
+		"DK": 21,
+		"FI": 17,
+		"SK": 18,
+		"IE": 21,
+		"HR": 23,
+		"LT": 23,
+		"SI": 24,
+		"LV": 20,
+		"EE": 22,
+		"CY": 21,
+		"LU": 25,
+		"MT": 20,
+	}
 }
 
 func getPort() string {
@@ -71,4 +143,20 @@ func getPort() string {
 		return "9000"
 	}
 	return port
+}
+
+func reduction(total float64) float64 {
+	reductNum := 0.0
+	if total >= 50000 {
+		reductNum = 0.15
+	} else if total >= 10000 {
+		reductNum = 0.1
+	} else if total >= 7000 {
+		reductNum = 0.07
+	} else if total >= 5000 {
+		reductNum = 0.05
+	} else if total >= 1000 {
+		reductNum = 0.03
+	}
+	return total - total*reductNum
 }
